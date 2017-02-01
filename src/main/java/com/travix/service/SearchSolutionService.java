f27f210537cf55d071270e899b00e476d14fa086
Alexandre da Silva <alexandre.silva@wplex.com.br>
2017-02-01 17:32:55 -0200

    Creating tests

--- src/main/java/com/travix/service/SearchSolutionService.java
+++ src/main/java/com/travix/service/SearchSolutionService.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,31 +13,40 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class SearchSolutionService
 {
-    private final String URL_CRAZY_AIR = "http://localhost:8090/crazyair";
+    private final String URL_CRAZY_AIR = "http://localhost:8090/crazyair/{request}";
 
-    private final String URL_TOUGH_JET = "http://localhost:8090/toughjet";
+    private final String URL_TOUGH_JET = "http://localhost:8090/toughjet/{request}";
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public SearchSolutionService()
+    {
+        this.restTemplate = new RestTemplate();
+    }
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/searchsolution/{request}", method = RequestMethod.GET)
-    public List<Map<String, String>> searchSolution()
+    public List<Map<String, String>> searchSolution(@PathVariable(value = "request")
+    final String requestJson)
     {
         final List<Map<String, String>> flights = new ArrayList<>();
 
-        final List<Map<String, String>> responseJsonCrazyAir = this.restTemplate.getForObject(this.URL_CRAZY_AIR, List.class);
+        final List<Map<String, String>> responseJsonCrazyAir = this.restTemplate.getForObject(this.URL_CRAZY_AIR, List.class, requestJson);
         System.out.println(responseJsonCrazyAir.get(0).toString());
 
-        final List<Map<String, String>> responseJsonToughJet = this.restTemplate.getForObject(this.URL_TOUGH_JET, List.class);
+        final List<Map<String, String>> responseJsonToughJet = this.restTemplate.getForObject(this.URL_TOUGH_JET, List.class, requestJson);
         System.out.println(responseJsonToughJet.get(0).toString());
+
+        //        final ObjectMapper mapper = new ObjectMapper();
+        //
+        //        List<Staff> list = mapper.readValue(json, new TypeReference(<List<FlightResponse>>(){});
 
         // TODO aqui devo transformar de json para objeto (lista), obter apenas como FlightResponse (objetos de retorno) e fazer o sort
 
         // ordenar lista por fare
 
-        flights.addAll(responseJsonCrazyAir);
-        flights.addAll(responseJsonToughJet);
+        //        flights.addAll(responseJsonCrazyAir);
+        //        flights.addAll(responseJsonToughJet);
 
         return flights;
     }
--- src/test/java/com/travix/SearchSolutionTest.java
+++ src/test/java/com/travix/SearchSolutionTest.java
@@ -1,13 +1,55 @@
 package com.travix;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.travix.model.FlightRequest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class SearchSolutionTest
 {
+    /**
+     * TODO valid the date ISO8601 and variables that has max numbers
+     */
+    private static final String BASE_URL = "http://localhost:8080/";
 
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setup()
+    {
+        this.restTemplate = new RestTemplate();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testValidSearchSolutionCrazyAir() throws JsonGenerationException, JsonMappingException, IOException
+    {
+        final String url = SearchSolutionTest.BASE_URL + "searchsolution/{request}";
+
+        final FlightRequest request = new FlightRequest("LHR", "LHR", "05-10-2010", "06-10-2013", 4);
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File("file.json"), request);
+
+        final String requestJson = mapper.writeValueAsString(request);
+
+        final List<Map<String, String>> responseJson = this.restTemplate.getForObject(url, List.class, requestJson);
+
+        System.out.println(responseJson);
+    }
 }
